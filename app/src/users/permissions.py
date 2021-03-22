2a9166371b2b0da132b8f81e00deb80fe916df60
Matvey <mat.coniaev2012@yandex.ru>
2021-03-22 22:07:47 +0300

    CP-3: Настроена единая авторизация пользователя

--- app/src/users/permissions.py
+++ app/src/users/permissions.py
@@ -18,33 +18,9 @@ GROUP_RIGHTS = {}
 
 """
 Список доступных прав, возможности:
-- permissions.login – вход в систему
-
-- permissions.system.settings – менять настройки системы
-
-- permissions.common.search – искать в системе что-либо
- 
 – permissions.users.index – просматривать пользователей
 – permissions.users.edit – редактировать пользователей
 
-- permissions.profile.index - просматривать свой профиль
-- permissions.profile.edit - редактировать свой профиль 
-
-- permissions.subjects.index - просматривать тематики обращений
-- permissions.subjects.edit - редактировать тематики обращений
-
-- permissions.links.index - просматривать полезные ссылки
-- permissions.links.edit - редактировать полезные ссылки
-
-- permissions.clients.index - просматривать клиентов
-- permissions.clients.edit - редактировать клиентов 
-- permissions.clients.card-activation - аткивировать карту
-
-- permissions.files.upload - загружать файлы
-- permissions.files.edit - изменять файлы (удалять)
-
-- permissions.product_categories.index - просматривать раздел категорий ЛП
-- permissions.product_categories.edit - изменять раздел категорий ЛП
 """
 
 GROUP_RIGHTS[GROUP_ADMINISTRATOR] = [
@@ -53,47 +29,10 @@ GROUP_RIGHTS[GROUP_ADMINISTRATOR] = [
 
     'permissions.users.index',
     'permissions.users.edit',
-
-    'permissions.profile.index',
-    'permissions.profile.edit',
-
-    'permissions.subjects.index',
-    'permissions.subjects.@admin',
-    'permissions.subjects.edit',
-
-
-    'permissions.clients.index',
-    'permissions.clients.edit',
-    'permissions.clients.card-activation',
-
-    'permissions.files.upload',
-    'permissions.files.edit',
-
-    'permissions.product_categories.index',
-    'permissions.product_categories.edit',
-
-    'permissions.search.index',
 ]
 
 GROUP_RIGHTS[GROUP_REVIEWER] = [
-    'permissions.profile.index',
-    'permissions.profile.edit',
 
-    'permissions.clients.index',
-    'permissions.clients.edit',
-    'permissions.clients.card-activation',
-
-    'permissions.files.upload',
-    'permissions.files.edit',
-
-    'permissions.subjects.index',
-
-    'permissions.links.index',
-
-    'permissions.product_categories.index',
-    'permissions.product_categories.edit',
-
-    'permissions.search.index',
 ]
 
 ADMIN_GROUP_RIGHTS = {}
