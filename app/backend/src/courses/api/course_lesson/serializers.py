35a634be93264f3cc3d56893df3ff49907463f3d
Matvey <mat.coniaev2012@yandex.ru>
2021-04-12 21:36:57 +0300

    CP-: Добавлено отслеживание прогресса прохождения урока

--- app/backend/src/courses/api/course_lesson/serializers.py
+++ app/backend/src/courses/api/course_lesson/serializers.py
@@ -4,6 +4,7 @@ from courses.api.lesson_fragment.serializers import (
     DetailLessonFragmentSerializers, DefaultLessonFragmentSerializers
 )
 from courses.models import CourseLesson
+from courses_access.common.models import AccessBase
 from courses_access.common.serializers import AccessBaseSerializers
 from courses_access.models.lesson_fragment import LessonFragmentAccess
 
@@ -17,13 +18,13 @@ class DefaultCourseLessonSerializers(AccessBaseSerializers):
 class DetailCourseLessonSerializers(DefaultCourseLessonSerializers):
     lesson_fragments = DefaultLessonFragmentSerializers(source='lessonfragment_set', many=True)
     accessible_lesson_fragments = serializers.SerializerMethodField()
+    progress = serializers.SerializerMethodField()
 
     def get_accessible_lesson_fragments(self, obj: CourseLesson) -> list:
         """
         Метод вернет все доступные фрагменты курса для пользователя
         :param obj: CourseLesson
         """
-        # TODO: Если у юзера есть доступ к курсу, то вернуть все фрагменты
         access_fragments = LessonFragmentAccess.objects.filter(
             lesson_fragment__course_lesson=obj, user=self.context.get('user')
         )
@@ -32,6 +33,20 @@ class DetailCourseLessonSerializers(DefaultCourseLessonSerializers):
         access_fragments = [f.lesson_fragment for f in access_fragments]
 
         return DetailLessonFragmentSerializers(access_fragments, many=True, context=self.context).data
+
+    def get_progress(self, obj: CourseLesson) -> float:
+        """
+        Возвращает целочисленный процент прогресса пользователя по уроку
+        :param obj: CourseLesson
+        """
+        user = self.context.get('user')
+
+        fragments = obj.lessonfragment_set.count()
+        completed_fragments = LessonFragmentAccess.objects.filter(
+            status=AccessBase.COURSES_STATUS_COMPLETED, lesson_fragment__course_lesson=obj, user=user,
+        ).count()
+
+        return completed_fragments / fragments * 100
 
     class Meta:
         model = CourseLesson
