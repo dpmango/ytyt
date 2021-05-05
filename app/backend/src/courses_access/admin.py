eeb39417b879abde94f190afd616fe5b10f5119b
Matvey <mat.coniaev2012@yandex.ru>
2021-05-05 21:40:23 +0300

    CP-: Убрана модель доступа

--- app/backend/src/courses_access/admin.py
+++ app/backend/src/courses_access/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from courses_access.models import Access
 
 
-@admin.register(Access)
-class AccessAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Access)
+# class AccessAdmin(admin.ModelAdmin):
+#     pass
