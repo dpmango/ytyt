a488c9e236aebeeeb3d276609c351ffae816708c
Matvey <mat.coniaev2012@yandex.ru>
2021-05-13 22:07:10 +0300

    CP-: Убран тестовый url

--- app/backend/src/crm/urls.py
+++ app/backend/src/crm/urls.py
@@ -4,13 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from crm.swagger import schema_view
-from users.views import test_template
 
 urlpatterns = [
     path('markdownx/', include('markdownx.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('dialog/', include('dialogs.urls')),
-    path('test-templates/', test_template),
     path('', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
