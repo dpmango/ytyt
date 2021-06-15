f78727c58549240c49e925dbb3cbf5079ef39e94
Matvey <mat.coniaev2012@yandex.ru>
2021-06-15 22:10:20 +0300

    CP-: django template max size img

--- app/backend/src/dialogs/templatetags/filter_size.py
+++ app/backend/src/dialogs/templatetags/filter_size.py
@@ -1,20 +1,8 @@
-from functools import wraps
-
 from django import template
 
 register = template.Library()
 
 
-def force_int(func):
-
-    @wraps(func)
-    def wrapper(*args):
-        func(*[int(arg) for arg in args])
-
-    return wrapper
-
-
 @register.filter(name='max_size')
-@force_int
 def max_size(width, width_max):
-    return width_max if width > width_max else width
+    return int(width_max) if int(width) > int(width_max) else int(width)
